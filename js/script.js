433526841e82e5986bfe420e034633c386b7bd3e
EnricoDG97 <enrico.delgaudio97@gmail.com>
2023-02-27 09:51:15 +0100

    feat: added script basilar code

--- js/script.js
+++ js/script.js
@@ -19,3 +19,15 @@
 //     scrivi solo buzz;
 // ALTRIMENTI
 //     scrivi solo il numero;
+
+for (let i = 1; i <=140; i++) {
+    if (i % 3 == 0 && i % 5 == 0) {
+        console.log("FizzBuzz");
+    } else if (i % 3 == 0) {
+        console.log("Fizz");
+    } else if (i % 5 == 0) {
+        console.log("Buzz");
+    } else {
+        console.log(i);
+    }
+}
